ef123338229ffee9b74ff47a13ed9025fb4fa089
Dan Stocker <dan@kwaia.com>
2012-11-11 12:40:37 +0100

    read-only related unit test removed for being depending on environment

--- js/Feature.test.js
+++ js/Feature.test.js
@@ -5,19 +5,6 @@
 (function (Feature) {
     module("Feature detection");
 
-    var browser = navigator.userAgent.match(/(\w+)(?=\/[\w\.]+)/g).pop().toLowerCase();
-
-    test("Covering RO properties", function () {
-        switch (browser) {
-        case 'firefox':
-            equal(Feature.canAssignToReadOnly(), false, "Can't assign to RO property in Firefox");
-            break;
-        case 'safari':
-            equal(Feature.canAssignToReadOnly(), true, "Can assign to RO property in Safari/Chrome");
-            break;
-        }
-    });
-
     test("Flags", function () {
         ok(troop.hasOwnProperty('testing'), "Testing flag exists");
         ok(troop.hasOwnProperty('writable'), "Writable flag exists");
