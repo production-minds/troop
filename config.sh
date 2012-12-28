18700d59138820cb3a0e3ee5b354423f156aecf1
Dan Stocker <dan@kwaia.com>
2012-12-28 14:45:55 +0100

    build process changed to builder

---
+++ config.sh
@@ -0,0 +1,14 @@
+LIB="troop"
+
+FILES="\
+js/license.js \
+js/troop.js \
+js/Polyfill.js \
+js/Feature.js \
+js/Base.js \
+js/Properties.js \
+js/Utils.js \
+js/Instantiation.js \
+js/Promise.js \
+js/exports.js \
+"
