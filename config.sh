91e66f0285a0fb3bf43a447e147d5c6596595b5d
Dan Stocker <dan@kwaia.com>
2012-12-28 21:52:03 +0100

    common build process 0.1

--- config.sh
+++ config.sh
@@ -12,3 +12,12 @@ js/Instantiation.js \
 js/Promise.js \
 js/exports.js \
 "
+
+PUSH=(
+evan/lib/troop
+flock/lib/troop
+kwaia/js/node_modules
+prime/lib/troop
+radiant/js/node_modules
+sntls/lib/troop
+)
