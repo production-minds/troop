cc90510cacc52652852c939bafc0ae819e820144
Dan Stocker <dan@kwaia.com>
2012-12-29 11:26:09 +0100

    common build 0.1.1

--- config.sh
+++ config.sh
@@ -12,12 +12,3 @@ js/Instantiation.js \
 js/Promise.js \
 js/exports.js \
 "
-
-PUSH=(
-evan/lib/troop
-flock/lib/troop
-kwaia/js/node_modules
-prime/lib/troop
-radiant/js/node_modules
-sntls/lib/troop
-)
