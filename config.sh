0bb06468882b547829f50e3fad0d3a412b587387
Dan Stocker <dan@kwaia.com>
2012-12-29 21:24:56 +0100

    config comment

--- config.sh
+++ config.sh
@@ -1,14 +1,19 @@
+#
+# Project configuration for Common Build
+# https://github.com/danstocker/builder
+#
+
 LIB="troop"
 
-FILES="\
-js/license.js \
-js/troop.js \
-js/Polyfill.js \
-js/Feature.js \
-js/Base.js \
-js/Properties.js \
-js/Utils.js \
-js/Instantiation.js \
-js/Promise.js \
-js/exports.js \
+FILES="
+js/license.js
+js/troop.js
+js/Polyfill.js
+js/Feature.js
+js/Base.js
+js/Properties.js
+js/Utils.js
+js/Instantiation.js
+js/Promise.js
+js/exports.js
 "
