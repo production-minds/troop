5297284bc274e9cf8be6e1c18b8128ad539e4626
Dan Stocker <dan@kwaia.com>
2012-12-30 16:06:01 +0100

    dependencies for Node

--- js/troop.js
+++ js/troop.js
@@ -1,4 +1,11 @@
 /**
- * Namespace
+ * Top-Level Library Namespace
  */
-var troop = {};
+/*global exports, require */
+var troop = {},
+    dessert;
+
+// adding Node.js dependencies
+if (typeof exports === 'object' && typeof require === 'function') {
+    dessert = require('dessert-0.2.1').dessert;
+}
