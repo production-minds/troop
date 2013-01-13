3395a6797add49c2b1ef9055662c69b06b1f3250
Dan Stocker <dan@kwaia.com>
2013-01-13 11:27:13 +0100

    2013 added to license

--- js/license.js
+++ js/license.js
@@ -1,1 +1,1 @@
-/* Copyright (c) 2012 Production Minds SD Limited, http://www.opensource.org/licenses/MIT */
+/* Copyright (c) 2012-2013 Production Minds SD Limited, http://www.opensource.org/licenses/MIT */
