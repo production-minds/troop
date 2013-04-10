fec45d24fdcbe2b0b105af52e530c20189789b5e
Dan Stocker <dan@kwaia.com>
2013-04-10 13:54:29 +0200

    dependency update

--- js/namespace.js
+++ js/namespace.js
@@ -4,9 +4,4 @@
 /*global require */
 /** @namespace */
 var troop = {},
-    dessert;
-
-// adding Node.js dependencies
-if (typeof require === 'function') {
     dessert = dessert || require('dessert').dessert;
-}
