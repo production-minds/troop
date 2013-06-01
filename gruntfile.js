7e0de4d2040b05731928c6f83914901b53cfe9f7
Dan Stocker <dan@kwaia.com>
2013-06-01 15:30:28 +0200

    grunt based target push

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,12 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        outPath           : 'out',
+        fileNameVersion   : '<%= pkg.name %>-<%= pkg.version %>.js',
+        filePath          : '<%= outPath %>/<%= pkg.name %>.js',
+        filePathVersion   : '<%= outPath %>/<%= pkg.name %>-<%= pkg.version %>.js',
+        filePathVersionMin: '<%= outPath %>/<%= pkg.name %>-<%= pkg.version %>.min.js',
+
         concat: {
             options: {
                 separator: ''
@@ -23,22 +29,26 @@ module.exports = function (grunt) {
                     'js/Promise.js',
                     'js/exports.js'
                 ],
-                dest: 'out/<%= pkg.name %>.js'
+                dest: '<%= filePath %>'
             }
         },
 
         min: {
             dist: {
-                src : ['out/<%= pkg.name %>.js'],
-                dest: 'out/<%= pkg.name %>-<%= pkg.version %>.min.js'
+                src : ['<%= filePath %>'],
+                dest: '<%= filePathVersionMin %>'
             }
         },
 
         copy: {
             main: {
                 files: [
-                    {src: 'out/<%= pkg.name %>.js', dest: 'out/<%= pkg.name %>-<%= pkg.version %>.js'}
+                    {src: '<%= filePath %>', dest: '<%= filePathVersion %>'}
                 ]
+            },
+
+            push: {
+                files: grunt.file.readJSON('targets.json')
             }
         },
 
@@ -68,7 +78,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-yui-compressor');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('build', ['test', 'concat', 'min', 'copy']);
+    grunt.registerTask('build', ['test', 'concat', 'min', 'copy:main']);
 
     grunt.registerTask('default', ['test']);
 };
