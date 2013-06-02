c397898d566df4c4790a091f27385ddd27005ce9
Dan Stocker <dan@kwaia.com>
2013-06-02 17:47:27 +0200

    single quotes in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,25 @@ module.exports = function (grunt) {
     "use strict";
 
     var params = {
-        "files": [
-            "js/license.js",
-            "js/namespace.js",
-            "js/Feature.js",
-            "js/Base.js",
-            "js/Memoization.js",
-            "js/Surrogate.js",
-            "js/Instantiation.js",
-            "js/Properties.js",
-            "js/Promise.js",
-            "js/exports.js"
+        files: [
+            'js/license.js',
+            'js/namespace.js',
+            'js/Feature.js',
+            'js/Base.js',
+            'js/Memoization.js',
+            'js/Surrogate.js',
+            'js/Instantiation.js',
+            'js/Properties.js',
+            'js/Promise.js',
+            'js/exports.js'
         ],
 
-        "test": [
-            "js/jsTestDriver.conf"
+        test: [
+            'js/jsTestDriver.conf'
         ],
 
-        "globals": {
-            "dessert": true
+        globals: {
+            dessert: true
         }
     };
 
