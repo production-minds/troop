211c3d3b2c94a4854b79f681c20857e6089ee035
Dan Stocker <dan@kwaia.com>
2013-06-02 12:48:27 +0200

    grunt process based on common-gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,30 @@
-/*global module */
+/*jshint node:true */
 module.exports = function (grunt) {
     "use strict";
 
-    var config = {
-        pkg: grunt.file.readJSON('package.json'),
+    var params = {
+        "files": [
+            "js/license.js",
+            "js/namespace.js",
+            "js/Feature.js",
+            "js/Base.js",
+            "js/Memoization.js",
+            "js/Surrogate.js",
+            "js/Instantiation.js",
+            "js/Properties.js",
+            "js/Promise.js",
+            "js/exports.js"
+        ],
 
-        config: grunt.file.readJSON('config.json'),
+        "test": [
+            "js/jsTestDriver.conf"
+        ],
 
-        outPath           : 'out',
-        fileNameVersion   : '<%= pkg.name %>-<%= pkg.version %>.js',
-        filePath          : '<%= outPath %>/<%= pkg.name %>.js',
-        filePathVersion   : '<%= outPath %>/<%= pkg.name %>-<%= pkg.version %>.js',
-        filePathVersionMin: '<%= outPath %>/<%= pkg.name %>-<%= pkg.version %>.min.js',
-
-        concat: {
-            options: {
-                separator: ''
-            },
-
-            dist: {
-                src : '<%= config.files %>',
-                dest: '<%= filePath %>'
-            }
-        },
-
-        min: {
-            dist: {
-                src : ['<%= filePath %>'],
-                dest: '<%= filePathVersionMin %>'
-            }
-        },
-
-        copy: {
-            main: {
-                files: [
-                    {src: '<%= filePath %>', dest: '<%= filePathVersion %>'}
-                ]
-            }
-        },
-
-        jshint: {
-            options: {
-                globals: '<%= config.globals %>',
-                ignores: ['js/**/*.test.js']
-            },
-
-            all: ['Gruntfile.js', 'js/**/*.js']
-        },
-
-        jstestdriver: {
-            files: '<%= config.test %>'
+        "globals": {
+            "dessert": true
         }
     };
 
-    /**
-     * Preparing targets
-     * Targets are a list of folders relative to the current folder
-     * to where the built file should be copied (versioned, uncompressed)
-     * Invoke a push by "> grunt copy:push"
-     */
-    var targets;
-    if (grunt.file.exists('targets.json')) {
-        targets = grunt.file.readJSON('targets.json')
-            .map(function (item) {
-                return     {
-                    expand: true,
-                    cwd   : '<%= outPath %>',
-                    src   : '<%= fileNameVersion %>',
-                    dest  : item
-                };
-            });
-
-        // adding push copy to config
-        config.copy.push = {files: targets};
-    }
-
-    grunt.initConfig(config);
-
-    // test-related tasks
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jstestdriver');
-    grunt.registerTask('test', ['jshint', 'jstestdriver']);
-
-    // build-related tasks
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-yui-compressor');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.registerTask('build', ['test', 'concat', 'min', 'copy:main']);
-
-    grunt.registerTask('default', ['test']);
+    // invoking common grunt process
+    require('common-gruntfile')(grunt, params);
 };
