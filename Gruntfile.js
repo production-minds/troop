52c14fb8593299df0969020b9ae4b51b4bd42a64
Dan Stocker <dan@kwaia.com>
2013-06-22 18:36:00 +0200

    common gruntfile updated to 0.1.2 & license moved inside package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function (grunt) {
 
     var params = {
         files: [
-            'js/license.js',
             'js/namespace.js',
             'js/Feature.js',
             'js/Base.js',
--- js/license.js
+++
@@ -1,1 +0,0 @@
-/*! Copyright (c) 2012-2013 Production Minds SD Limited, http://www.opensource.org/licenses/MIT */
