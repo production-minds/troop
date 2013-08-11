d55f9720460e2bbaa269c89fa5d31c3af704d704
Dan Stocker <dan@kwaia.com>
2013-08-11 11:16:32 +0200

    merges wiki into Postpone

--- js/Postpone.js
+++ js/Postpone.js
@@ -4,7 +4,7 @@
  * API to add properties to objects so that they won't get evaluated until
  * first access.
  */
-/*global dessert, troop, console */
+/*global dessert, troop */
 (function () {
     "use strict";
 
@@ -12,10 +12,20 @@
 
     troop.Base.addMethods.call(troop, /** @lends troop */{
         /**
-         * Adds a postponed property definition (read-only).
+         * Postpones a property definition on the specified object until first access.
+         * Initially assigns a special getter to the property, then, when the property is accessed for the first time,
+         * the property is assigned the return value of the generator function, unless a value has been assigned from
+         * within the generator.
          * @param {object} host Host object.
          * @param {string} propertyName Property name.
-         * @param {function} generator Generates (and returns) property value.
+         * @param {function} generator Generates (and returns) property value. Arguments: host object, property name,
+         * plus all extra arguments passed to .postpone().
+         * @example
+         * var obj = {};
+         * troop.postpone(obj, 'foo', function () {
+         *    return "bar";
+         * });
+         * obj.foo // runs generator and alerts "bar"
          */
         postpone: function (host, propertyName, generator) {
             dessert
